8bdab484c50b24010f928da61216bdc69ee772f9
Esperanza Duenas <da13002@ues.edu.sv>
2019-09-01 22:45:45 -0600

    faltan validaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+//import para corregir error en consola
+import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+//no olvidar declarar el formulario
+export class AppComponent implements OnInit{
   title = 'formulario-reactivo';
+  //mismo nombre que el que esta definido en el html
+  reactiveForm : FormGroup;
+
+  /**
+   *Constructor de la clase, por el momento vacio
+   */
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit(){this.crearForm();}
+
+  crearForm(){
+    this.reactiveForm = this.fb.group({
+      //se declaran los name que tienen los input en el formulario
+      txtNombre : [''],
+      txtEmail : [''],
+      txtPassword : ['']
+    });
+  }
+
+  onSubmit(){
+    console.log("Has accionado un evento", this.reactiveForm.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//ReactiveForms
+import {ReactiveFormsModule} from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    //no olvidar agregar el import a @ngModule
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
